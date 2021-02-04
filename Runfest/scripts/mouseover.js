4b0b3b2c6be6f39806e531bb6249a4a578b61a40
icedvigilante <wm.ryan.mooney@gmail.com>
2021-02-03 17:46:40 -0900

    add sponsors section with hover for image and filters

--- Runfest/scripts/mouseover.js
+++ Runfest/scripts/mouseover.js
@@ -1,13 +1,37 @@
-$('.circle').on('mouseover', function() {
-    $(this).stop().animate({
-        'border-width': 1,
-        'border-radius': 200
-    }, 500);
-});
+// $('.circle').on('mouseover', function() {
+//     $(this).stop().animate({
+//         'border-width': 1,
+//         'border-radius': 200
+//     }, 500);
+// });
+//
+// $('.circle').on('mouseout', function() {
+//     $(this).stop().animate({
+//         'border-width': 0,
+//         'border-radius': 100
+//     }, 500);
+// });
 
-$('.circle').on('mouseout', function() {
-    $(this).stop().animate({
-        'border-width': 0,
-        'border-radius': 100
-    }, 500);
-});
+$(document).ready(function() {
+    $("[data-fancybox]").fancybox();
+
+    $("#filters a").click(function() {
+        $("#filters .current").removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+
+    });
+})
+
